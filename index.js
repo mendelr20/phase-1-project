a8e3924cd1fbe74be1046cd4bbea972575c36020
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2022-05-05 12:53:57 -0400

    Search bar added and working

--- index.js
+++ index.js
@@ -1,109 +1,94 @@
-
-// JSON link
-const url = "http://localhost:3000/drugs";
-
-//HTML Links
-const tab = document.querySelector('.tab');
-const info = document.querySelector('#info');
-
-
-
-fetchRequest()
-// Fetch Request function
-function fetchRequest(){
-    return fetch(url)
-    .then(res => res.json())
-    .then(arrayOfDrugs => {
-        arrayOfDrugs.forEach(drug => appendDomDrugList(drug))
-    })
-}
-
-
-// Append the drugs to the dom on the left side
-function appendDomDrugList(drug){
-    const button = document.createElement('button');
-    button.innerText = drug.name;
-    button.setAttribute('class', 'btn');
-    tab.appendChild(button);
-    // Event listener for a drug is clicked.
-    button.addEventListener('click', () => {
-        // Call back function for when a drug is clicked (did it like this in order to pass in the drug)
-        buttonCallBack(drug);
-    })
-}
-
-// Callback for when the drug is clicked
-function buttonCallBack(drug){
-    // Empty info 
-    info.innerHTML= '';
-    const name = document.createElement('h1');
-    name.innerHTML=`${drug.name}`;
-
-    // Create a element for drug class and add the content
-    const drugClass = document.createElement('h3');
-    drugClass.innerHTML=` Drug Class: <br> ${drug.class}`;
-
-    // Create a element for mechanism of action and add the content
-    const mechanismOfAction = document.createElement('h3');
-    mechanismOfAction.innerHTML=` Mechanism Of Action: <br> ${drug.mechanismOfAction}`;
-
-    // Create a element for indications and add the content
-    const indications = document.createElement('h3');
-    indications.innerHTML = `Indications: <br> ${drug.indications}`;
-
-    // Create a element for contraindications and add the content
-    const contraindications = document.createElement('h3');
-    contraindications.innerHTML = `Contraindications: <br> ${drug.contraindications}`;
-
-    // Create a element for Adverse Reactions and add the content
-    const adverseReactions = document.createElement('h3');
-    adverseReactions.innerHTML = `Adverse Reactions: <br> ${drug.adverseReactions}`;
-
-    // Create a element for Drug Interactions and add the content
-    const drugInteractions = document.createElement('h3');
-    drugInteractions.innerHTML = `Drug Interactions: <br> ${drug.drugInteractions}`;
-
-    // Create a element for Dosage and Administration and add the content
-    const dosageAndAdministration = document.createElement('h3');
-    dosageAndAdministration.innerHTML = `Dosage and Administration:`;
-
-    // append all the info up to dosage and administration to the dom
-    info.append(name, drugClass, mechanismOfAction, indications, contraindications, adverseReactions, drugInteractions, dosageAndAdministration)
-
-    // Create a section for each protocol that exits it will 
-    for(const protocol in drug.dosageAndAdministration){
-        console.log(protocol)
-        // Create a section for each protocol
-        for(const age in protocol){
-           
-        }
-    }
+// Differs running javascript until all html is loaded
+document.addEventListener('DOMContentLoaded', () => {
     
-}
+    // JSON link
+    const url = "http://localhost:3000/drugs";
+
+    //HTML Links
+
+    // Link to the list of drugs
+    const tab = document.querySelector('.tab');
+    // Link to the info section
+    const info = document.querySelector('#info');
+    // Link to the search bar
+    const search = document.querySelector('#searchbar')
+    // Link to buttons
+    const btns = document.getElementsByClassName('btn')
+        
+    // Fetch request function
+    function fetchRequest(){
+        // Fetch with the url passed in
+        fetch(url)
+        // Take the response and convert it to JSON
+        .then(res => res.json())
+        // Take the JSON file
+        .then(arrayOfDrugs => {
+            // Take the array of drugs and pass it into a for each loop that will iterate through each drug and pass it into the function that will append it to the DOM
+            arrayOfDrugs.forEach(drug => appendDomDrugList(drug))
+        })
+    }
+
+    // Function to append the drugs to the dom on the left side
+    function appendDomDrugList(drug){
+        // Create a button
+        const button = document.createElement('button');
+        // Set the buttons inner text to the drugs name
+        button.innerText = drug.Name;
+        // Set the btns class to btn - will be needed for the search bar
+        button.setAttribute('class', 'btn');
+        // Append the button to the tab section
+        tab.appendChild(button);
+        // Event listener for a drug when it is clicked.
+        button.addEventListener('click', () => {
+            // Call back function for when a drug is clicked (did it like this in order to pass in the drug)
+            buttonCallBack(drug);
+        })
+    }
+
+    // Function call back for when a drug is clicked
+    function buttonCallBack(drug){
+        // Empty the info section
+        info.innerHTML = ""
+        // Iterate through each property in the drug and create and append its info
+        for (const property in drug) {
+            // Create a H3
+            const h3 = document.createElement('h3')
+            // Set the H3 inner HTML to property and its information and add a line at the end before the next one
+            h3.innerHTML = `${property}: ${drug[property]} \n`
+            // Append the H3 to the info section
+            info.append(h3)
+          }
+    }
+
+    // Event listener for when the search bar detects any change - which is why a input event is used as it fire as soon as any change is detected
+    search.addEventListener('input', () => {
+        // Set input to = whatever is entered into the search bar but changed to lower case
+        const input = search.value.toLowerCase()
+        // Iterate through each button 
+        for (i = 0; i < btns.length; i++) { 
+            // If the buttons inner HTML in lower case does not include the search input it will hide it via CSS
+            if (!btns[i].innerHTML.toLowerCase().includes(input)) {
+                btns[i].style.display="none";
+            }
+            else {
+                // Else it will keep it displayed
+                btns[i].style.display="list-item";                 
+            }
+        }
+        
+    })
+
+
+    // Run the fetch request
+    fetchRequest()
+})
 
 
 
 
 
-// // If NREMT exists then create the section for the dosage and administration for it
-// if (drug.dosageAndAdministration.nremt){
-//     // Create a element for NREMT dosing, add the content, append it to the dom
-//     const nremt = document.createElement('h4');
-//     nremt.innerHTML = `NREMT:`;
-//     info.append(nremt)
 
-//     // If adult dosages exists create the section for adult doses
-//     if(drug.dosageAndAdministration.nremt.adult){
-//         // Create a element for adult
-//         const nremtAdult = document.createElement('h4');
-//         nremtAdult.innerHTML = `Adult:`;
 
-//         // Create a element for 1st Dose
-//         const nremtAdult1stDose = document.createElement('h5');
-//         nremtAdult1stDose.innerHTML = `1st Dose: <br> ${drug.dosageAndAdministration.nremt.adult.firstDose.dose} ${drug.dosageAndAdministration.nremt.adult.firstDose.unitType}<br> ${drug.dosageAndAdministration.nremt.adult.firstDose.howItIsAdministered}`
-//         info.append(nremtAdult, nremtAdult1stDose)
-//         for (const dose in drug.dosageAndAdministration.nremt.adult) {
-//             console.log(dose);
-//           }
-//     }
-// }
+
+
+
